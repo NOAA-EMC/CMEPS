3cb145f6a937e1cc7b8f5811931146edf36911d4
denise.worthen <Denise.Worthen@noaa.gov>
2025-04-08 13:12:52 -0500

    fix failing datm and hafs rts

--- mediator/med_internalstate_mod.F90
+++ mediator/med_internalstate_mod.F90
@@ -690,18 +690,16 @@ contains
     if (is_local%wrap%comp_present(compice)) defaultMasks(compice,:) = 0
     if (is_local%wrap%comp_present(compwav)) defaultMasks(compwav,:) = 0
     if ( coupling_mode(1:3) == 'ufs') then
-       if (is_local%wrap%comp_present(compatm)) then
-          if (atm_name(1:4) == 'datm') then
-             defaultMasks(compatm,1) = 0
-          else
-             defaultMasks(compatm,2) = 1
-          end if
-       end if
+       if (is_local%wrap%comp_present(compatm)) defaultMasks(compatm,2) = 1
     endif
-    if ( coupling_mode(1:4) == 'hafs') then
+    if ( trim(coupling_mode) == 'hafs') then  ! not hafs.mom6
        if (is_local%wrap%comp_present(compatm)) defaultMasks(compatm,1) = 1
     endif
-
+    if ( coupling_mode /= 'cesm') then
+       if (is_local%wrap%comp_present(compatm) .and. atm_name(1:4) == 'datm') then
+          defaultMasks(compatm,1) = 0
+       end if
+    end if
   end subroutine med_internalstate_defaultmasks
 
 end module med_internalstate_mod
