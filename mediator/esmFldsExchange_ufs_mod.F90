1b26fe9f94cda18b6b8331ece36f1de0abd0ed41
Denise Worthen <denise.worthen@noaa.gov>
2025-05-09 05:35:15 -0400

    allow datm configs to map with patch or bilnr

--- mediator/esmFldsExchange_ufs_mod.F90
+++ mediator/esmFldsExchange_ufs_mod.F90
@@ -656,11 +656,7 @@ contains
        else
           if ( fldchk(is_local%wrap%FBexp(compice)        , fldname, rc=rc) .and. &
                fldchk(is_local%wrap%FBImp(compatm,compatm), fldname, rc=rc)) then
-             if (med_aoflux_to_ocn) then
-                call addmap_from(compatm, fldname, compice, maptype, 'one', 'unset')
-             else
-                call addmap_from(compatm, fldname, compice, mapbilnr, 'one', 'unset')
-             end if
+             call addmap_from(compatm, fldname, compice, mapbilnr, 'one', 'unset')
              call addmrg_to(compice, fldname, mrg_from=compatm, mrg_fld=fldname, mrg_type='copy')
           end if
        end if
@@ -679,14 +675,10 @@ contains
        else
           if ( fldchk(is_local%wrap%FBexp(compice)        , fldname, rc=rc) .and. &
                fldchk(is_local%wrap%FBImp(compatm,compatm), fldname, rc=rc)) then
-             if (med_aoflux_to_ocn) then
-                call addmap_from(compatm, fldname, compice, maptype, 'one', 'unset')
+             if (mapuv_with_cart3d) then
+                call addmap_from(compatm, fldname, compice, mappatch_uv3d, 'one', 'unset')
              else
-                if (mapuv_with_cart3d) then
-                   call addmap_from(compatm, fldname, compice, mappatch_uv3d, 'one', 'unset')
-                else
-                   call addmap_from(compatm, fldname, compice, mappatch, 'one', 'unset')
-                end if
+                call addmap_from(compatm, fldname, compice, mappatch, 'one', 'unset')
              end if
              call addmrg_to(compice, fldname, mrg_from=compatm, mrg_fld=fldname, mrg_type='copy')
           end if
